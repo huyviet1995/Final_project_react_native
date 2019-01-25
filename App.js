7a85ca26f8774278fa50d365556d744f549da140
vdang95 <vdang@gustavus.edu>
2019-01-25 18:25:01 +0700

    Create the basic structure for the app including Product List

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button,StyleSheet, Text, View} from 'react-native';
+import {database} from './Firebase.js'
+import {AppContainer} from './AppContainer';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product_price: 0,
+    }
+  }
+
+  fetchProducts = (tableName) => {
+    const table_path = "/" + tableName; 
+    const table_ref = database.ref(table_path);
+    table_ref.on('value',(snapshot) => {
+      console.log("Read from the database");
+      console.log(snapshot.val());
+      this.setState({
+        product_price: snapshot.val(),
+      })
+    });
+  }
+
+
+   pushProducts = (tableName, first_name, last_name, email) => {
+    const table_path = "/" + tableName;
+    const table_ref = database.ref(table_path);
+    const new_user = {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+    }
+    table_ref.push(new_user).then((data) => {
+      console.log("Write to database");
+      console.log("data",data);
+    }).catch((error) => {
+      console.log('error ' + error);
+    })
+  }
+
+  goToAddProduct = () => {
+    this.props.navigation.navigate("AddProduct");
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text> Welcome to my final application! </Text>
-      </View>
+      <AppContainer/>
     );
   }
 }
@@ -14,8 +54,12 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    color: 'white',
+    backgroundColor: 'red'
+  }
 });
